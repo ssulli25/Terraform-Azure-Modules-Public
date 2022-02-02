a21121d35216d6ea59d7b8eb6ba1f0dd06d9f1a1
ssulli25 <seasulli25@gmail.com>
2022-02-01 18:40:03 -0600

    Updated variables for app gw - spacing.

--- Application_Gateway/variables.tf
+++ Application_Gateway/variables.tf
@@ -2,18 +2,23 @@
 variable "resource_group_name" {
   description = "Name of the resource group to place App Gateway in."
 }
+
 variable "appgw_name" {
   description = "Name of App Gateway"
 }
+
 variable "ag_vnet_name" {
   description = "Name of Virtual Network that the App Gateway will be placed."
 }
+
 variable "ag_subnet_id" {
   description = "Subnet Id of Application Gateway."
 }
+
 variable "fe_ip_config_name" {
   description = "(Required) The Name of the Frontend IP Configuration used for this HTTP Listener."
 }
+
 // Optional
 variable "backend_address_pools" {
   description = "List of backend address pools."
@@ -23,41 +28,50 @@ variable "backend_address_pools" {
     fqdns        = list(string)
   }))
 }
+
 variable "backend_http_settings" {
   description = "List of backend HTTP settings."
   type = any
 }
+
 variable "http_listeners" {
   description = "List of HTTP/HTTPS listeners. HTTPS listeners require an SSL Certificate object."
   type = any
 }
+
 variable "basic_request_routing_rules" {
   description = "Request routing rules to be used for listeners."
   type = any
   default = []
 }
+
 variable "redirect_request_routing_rules" {
   description = "Request routing rules to be used for listeners."
   type = any
   default = []
 }
+
 variable "path_based_request_routing_rules" {
   description = "Request routing rules to be used for listeners."
   type = any
   default = []
 }
+
 variable "ag_sku_name" {
   description = "Name of App Gateway SKU. Options include Standard_Small, Standard_Medium, Standard_Large, Standard_v2, WAF_Medium, WAF_Large, and WAF_v2"
   default     = "Standard_v2"
 }
+
 variable "ag_sku_tier" {
   description = "Tier of App Gateway SKU. Options include Standard, Standard_v2, WAF and WAF_v2"
   default     = "Standard_v2"
 }
+
 variable "capacity" {
   description = "(Required) The Capacity of the SKU to use for this Application Gateway. When using a V1 SKU this value must be between 1 and 32, and 1 to 125 for a V2 SKU. This property is optional if autoscale_configuration is set."
   default     = 2
 }
+
 variable "frontend_ports" {
   description = "A list of frontend_port blocks as defined below."
   default     = []
@@ -66,11 +80,13 @@ variable "frontend_ports" {
     port                                      = number
   }))
 }
+
 variable "probes" {
   description = "Health probes used to test backend health."
   default     = []
   type = any
 }
+
 variable "url_path_maps" {
   description = "URL path maps associated to path-based rules."
   default     = []
@@ -125,18 +141,22 @@ variable "ssl_certificates" {
 variable "ag_public_ip_name" {
   description = "Public IP of App GW."
 }
+
 variable "ag_allocation_method" {
   description = "(Required) Defines the allocation method for this IP address. Possible values are Static or Dynamic."
   default     = "Static"
 }
+
 variable "ag_public_ip_sku" {
   description = "(Optional) The SKU of the Public IP. Accepted values are Basic and Standard. Defaults to Basic."
   default     = "Standard"
 }
+
 variable "ag_domain_name_label" {
   description = "Domain name label for Public IP created."
   default = null
 }
+
 variable "ag_availability_zone" {
   description = "(Optional) The availability zone to allocate the Public IP in. Possible values are Zone-Redundant, 1, 2, 3, and No-Zone. Defaults to Zone-Redundant."
   default = "Zone-Redundant"
